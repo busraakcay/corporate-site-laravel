4636686add326f63a49812bbf84ce4d1d064dbee
busraakcay <0busraakcay@gmail.com>
2024-08-12 23:32:30 +0300

    conf 4

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'merlynshoe'),
-            'username' => env('DB_USERNAME', 'rootlocalhost'),
+            'username' => env('DB_USERNAME', 'merlynroot'),
             'password' => env('DB_PASSWORD', 'f8nO7n3&3'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
